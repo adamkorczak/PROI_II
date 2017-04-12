a0d71dee25686f122de523a3e858586ef27c9707
Adam-Korczak <adam-korczak@debian-akor.akor.com>
2017-04-12 02:05:01 +0200

    functions split, insert

--- main.cpp
+++ main.cpp
@@ -7,29 +7,35 @@
 template <class typ>
 class treeNode
 {
+public:
+
 	typ *keys;
 	int minDegree;
 	int index;
-	int curr_d;
+	int currDeg;
 	bool leaf;
 	
 	treeNode *up;
 	treeNode **child;
 
-	typ data;
+	typ *data;
 
-public:
+
 
 	treeNode(int _d = 1, bool _leaf = true, int _index = 1)
 		:	minDegree(_d), leaf(_leaf), index(_index)
 		{	keys = new typ [2*_d - 1];
+			data = new typ [2*_d - 1];
 			child = new treeNode *[2*_d]; 
-			curr_d = 0;
+			currDeg = 0;
+		//std::cout <<"konstruktor" << std::endl;
 		}
 	
 
-	treeNode *search(int _k);
+	treeNode *search(typ _k);
 	void traverse();
+	void insertInEmptySlots(typ _data, typ _k);
+	void split(int m, treeNode<typ> *ptr);
 
 };
 
@@ -46,14 +52,130 @@ public:
 		: minDeg(_minDeg) { root = NULL; }
 
 	void traverse();
-	treeNode<typ>* search(int _k);
-	
-	
-	
+	treeNode<typ>* search(typ _k);
+	void insert(typ _data, typ _k);
+		
 };
 
+
 template <class typ>
-treeNode<typ>* bTree<typ>::search(int _k)
+void bTree<typ>::insert(typ _data, typ _k)
+{
+	if(root == NULL)
+	{
+		root = new treeNode<typ>(minDeg,true,0);
+		root ->keys[0] = _k;
+		root ->data[0] = _data;
+		root ->currDeg = 1;
+
+	}
+	else
+	{
+		if(root->currDeg == (2*minDeg - 1))
+		{
+				treeNode<typ> *p = new treeNode<typ>(minDeg,false,0);
+				
+				p ->child[0] = root;
+				p ->split(0, root);
+
+				int i = 0;
+				if(p->keys[0] < _k)
+					i++;
+
+				p->child[i]->insertInEmptySlots(_data,_k);		
+				root = p;						
+		}
+		else
+		{
+			root->insertInEmptySlots(_data, _k);
+			//std::cout<< "wstawiam do pustych" << std::endl;
+		}
+		
+	
+	}
+
+
+}
+
+template <class typ>
+void treeNode<typ>::insertInEmptySlots(typ _data, typ _k)
+{
+int i = currDeg-1;
+
+	if(leaf == true)
+	{
+		while( i >= 0 && keys[i] > _k)
+		{
+			keys[i+1] = keys[i];
+			i--;
+		} 
+		
+		keys[i+1] = _k;
+		data[i+1] = _data;
+		currDeg = currDeg + 1;
+
+	}
+	else
+	{
+		while( i >= 0 && keys[i] > _k)
+		i--;
+
+		if(child[i+1]->currDeg == (2*minDegree - 1))
+		{
+			split(i+1, child[i+1]);
+
+			if(keys[i+1] < _k)
+			i++;
+		}
+		child[i+1]->insertInEmptySlots(_data, _k);
+	}
+
+}
+
+
+template <class typ>
+void treeNode<typ>::split(int m, treeNode<typ> *ptr)
+{
+
+	treeNode<typ> *s = new treeNode<typ>(ptr->minDegree,  ptr->leaf,0);
+	s->currDeg = (minDegree - 1);
+
+	for(int j = 0; j < (minDegree - 1); j++)
+	{
+		s->keys[j] = ptr->keys[j+minDegree];
+		s->data[j] = ptr->data[j+minDegree];
+	}
+
+	if(ptr->leaf == false)
+	{
+		for(int j = 0; j < minDegree; j++)
+		s->child[j] = ptr->child[j+minDegree];
+	}
+	
+	ptr->currDeg = (minDegree - 1);
+
+	for(int j = currDeg; j >= (m + 1); j--)
+	{
+		child[j+1] = child[j];
+	}
+
+	child[m+1] = s;
+
+	for(int j = (currDeg - 1); j >= m; j--)
+	{
+		keys[j+1] = keys[j];
+		data[j+1] = data[j];
+	}
+
+	keys[m] = ptr->keys[minDegree - 1];
+	data[m] = ptr->data[minDegree - 1];
+
+	currDeg = currDeg + 1;
+}
+
+
+template <class typ>
+treeNode<typ>* bTree<typ>::search(typ _k)
 {
 	if(root == NULL)
 	return NULL;
@@ -74,24 +196,27 @@ template <class typ>
 void treeNode<typ>::traverse()
 {
 	int i;
-	for(i= 0; i < curr_d; i++)
+	for(i= 0; i < currDeg; i++)
 	{
 		if(leaf == false)
-		child[i] ->traverse();
-		
-		std::cout << " " << keys[i];
-	
+		{
+			child[i] ->traverse();
+		}
+		std::cout << " " << keys[i];	
 	}
 
-	if(leaf = false)
-	child[i] -> traverse();
+
+	if(leaf == false)
+	{
+		child[i] -> traverse();
+	}
 
 }
 template <class typ>
-treeNode<typ> *treeNode<typ>::search(int _k)
+treeNode<typ> *treeNode<typ>::search(typ _k)
 {
 	int i = 0;
-	while( i < curr_d && _k > keys[i])
+	while( i < currDeg && _k > keys[i])
 		i++;
 
 	if(keys[i] == _k)
@@ -107,6 +232,16 @@ treeNode<typ> *treeNode<typ>::search(int _k)
 int main()
 {
 
+srand(time(NULL));
+
+bTree<int> A(5);
+int a;
+	for(a = 0; a < 100; a++)
+	{
+		A.insert(a,a);
+	}
+	A.traverse();
+	std::cout << std::endl;
 
 	return 0;
 }
